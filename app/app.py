f3754261cfb87408c7f12ab4c1d1af6d3fa3462f
Raj <rajshivage@gmail.com>
2022-07-23 06:22:29 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ def index():
     """
     Function to test the functionality of the API
     """
-    return "Hello, world!"
+    return "Hello, World!"
 
 
 @app.route("/authors", methods=["POST"])
